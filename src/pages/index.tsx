2654508ec2deb75074fc9900fb406d724c7f0a25
Valentino Conti <valeen.conti@hotmail.com>
2023-04-20 12:50:29 -0300

    Source and Destination chains changing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,16 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.scss";
+import { useState } from "react";
+import { IChain } from "@/types";
+import Chain from "@/components/Chain";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [source, setSource] = useState<IChain>("AVAX");
+  const changeSource = () => setSource(source === "AVAX" ? "ETH" : "AVAX");
+
   return (
     <>
       <Head>
@@ -16,17 +22,35 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.center}>
-          <div className={styles.container}>HOLAA</div>
+        <div className={`${styles.center} ${inter.className}`}>
+          <h2 className={styles.title}>USDC Bridge</h2>
 
-          {/* <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          /> */}
+          <div className={styles.container}>
+            <div className={styles.chainText}>Source</div>
+            <Chain source={source} initial="AVAX" />
+
+            <div
+              className={styles.exchange}
+              onClick={changeSource}
+              style={{
+                transform: `rotate(${source === "AVAX" ? 90 : 270}deg)`,
+              }}
+            >
+              <Image
+                alt="Exchange source and destination chains"
+                src="/exchange.png"
+                width={30}
+                height={30}
+              />
+            </div>
+
+            <div className={styles.chainText}>Destination</div>
+            <Chain source={source} initial="ETH" />
+
+            <button>Connect Wallet</button>
+
+            {/* <div>Source Balance</div> */}
+          </div>
         </div>
       </main>
     </>
