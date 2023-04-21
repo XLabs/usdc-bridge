39688521a4522334ec66747ab07cfe7748c7f848
Valentino Conti <valeen.conti@hotmail.com>
2023-04-21 12:22:16 -0300

    cleanup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const isDeploying = process.env.DEPLOY === "deploy";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isDeploying ? "/usdc-bridge" : "",
+  basePath: isDeploying ? "/usdc-bridge" : "",
+  images: {
+    unoptimized: isDeploying ? true : false
+  },
+  output: 'export'
 }
 
 module.exports = nextConfig
