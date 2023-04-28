841438f179444f1d5c386e57c96cfa3f81804c40
Valentino Conti <valeen.conti@hotmail.com>
2023-04-28 12:03:34 -0300

    Fix loader not showing fully on the button

--- src/components/atoms/Loader/index.tsx
+++ src/components/atoms/Loader/index.tsx
@@ -7,7 +7,11 @@ type Props = {
 const Loader = ({ size = "l" }: Props) => (
   <div
     className={styles.spinnerLoaderContainer}
-    style={{ transform: `scale(${size === "m" ? 0.6 : 1})` }}
+    style={{
+      transform: `scale(${size === "m" ? 0.6 : 1}) translateY(-${
+        size === "m" ? 6 : 0
+      }px)`,
+    }}
   >
     <span className={styles.spinnerLoader} />
   </div>
