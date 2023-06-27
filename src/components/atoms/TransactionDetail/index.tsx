fbc350f23b886a913e2efeb7f1703d2d5b08cd23
Valentino Conti <valeen.conti@hotmail.com>
2023-06-27 00:55:39 -0300

    Fix final received amount

--- src/components/atoms/TransactionDetail/index.tsx
+++ src/components/atoms/TransactionDetail/index.tsx
@@ -10,14 +10,10 @@ type Props = {
 };
 
 const TransactionDetail = ({ amount, estimatedGas, destination, destinationGas, transactionFee }: Props) => {
-  const USDCamount = (+amount - +destinationGas).toFixed(5);
+  const USDCamount = (+amount - +destinationGas - +transactionFee).toFixed(5);
 
   return (
     <div className={styles.transactionDetail}>
-      <div className={styles.infoLine}>
-        <span>You receive</span>
-        <span>{`${USDCamount} USDC`}</span>
-      </div>
       <div className={styles.infoLine}>
         <span>Est. Destination Gas</span>
         <span>{estimatedGas ? `≈${estimatedGas} ${getChainNativeTokenName(destination)}` : "..."}</span>
@@ -25,6 +21,10 @@ const TransactionDetail = ({ amount, estimatedGas, destination, destinationGas, 
       <div className={styles.infoLine}>
         <span>Relay Cost</span>
         <span>{transactionFee === "" ? "0.00" : "≈" + transactionFee} USDC</span>
+      </div>
+      <div className={styles.infoLine}>
+        <span>You receive</span>
+        <span>{`${USDCamount} USDC`}</span>
       </div>
     </div>
   );
